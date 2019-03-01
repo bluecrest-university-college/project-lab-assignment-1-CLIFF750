1cdc9cf48934f482fd40850c135e51b734b5f661
CLIFF750 <48007610+CLIFF750@users.noreply.github.com>
2019-03-01 23:09:49 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,128 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        using namespace std;
+        int main;
+        {
+        vector intList;
+        }
+
+
+class addressType
+{
+    private;
+    char name = 500;
+    long phone_num; //class addressType :public addressbook
+    char address = 500 ;
+    int year;
+    int month;
+    int day;
+    char type;
+    int FM,F,B;
+    char Family,Friend,Business;// char friend[];
+
+    public;
+    addressType;
+    void setName;
+    void getName;
+    void getPhone;
+    long setPhone;
+    varchar setAddress;
+    void getAddress;
+    float getAverage;
+    int setDay();
+    void addressType()::int getDay();
+
+
+    addressType addressType(){
+    st_address = "";
+    city = "";
+    state = "";
+    zip = 0;
+}
+
+    {
+        cout<<"\tBIRTH DAY : "<}
+
+
+    int setMonth;
+    void getMonth;
+    int setYear;
+    void getYear;
+    char setType;
+    void getType;
+};
+
+
+class personType // as per 1-12
+{
+    public:
+    void print() const;
+//Function to output the first name and last name
+//in the form of firstName lastName.
+
+    void setName(string first, string last);
+//Function to set firstName and lastName according
+//to the parameters.
+//Postcondition: firstName = first; lastName = last
+
+    string getFirstName() const;
+//Function to return the first name.
+//Postcondition: The value of the data member firstName
+// is returned.
+
+    string getLastName()const;
+    //Function to return the last name.
+//Postcondition: The value of the data member lastName
+// is returned.
+    int setMonth;
+    void getMonth;
+    int setYear;
+    void getYear;
+    char setType;
+    void getType;
+};
+
+
+class personType // as per 1-12
+{
+    public:
+    void print;
+//Function to output the first name and last name
+//in the form of firstName lastName.
+
+    void setName first, last;
+//Function to set firstName and lastName according
+//to the parameters.
+//Postcondition: firstName = first; lastName = last
+
+    string getFirstName() const;
+//Function to return the first name.
+//Postcondition: The value of the data member firstName
+// is returned.
+
+    string getLastName() const;
+//Function to return the last name.
+//Postcondition: The value of the data member lastName
+// is returned.
+
+    void printDate const ();
+//Function to output the date in the form mm-dd-yyyy.
+
+   int dateType(int month = 1; int day = 1; int year = 1900;
+//constructor to set the date
+//The data members dMonth, dDay, and dYear are set
+//according to the parameters.
+//Postcondition: dMonth = month; dDay = day;
+// dYear = year
+//If no values are specified, the default values are
+//used to intialize the data members.
+
+    private;
+    int dMonth; //variable to store the month
+    int dDay; //variable to store the day
+    int dYear; //variable to store the year
+};
